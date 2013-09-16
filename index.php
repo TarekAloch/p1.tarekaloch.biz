e893a5664dede3390490b1eca79305a862f641a9
TarekAloch <taloch@fas.harvard.edu>
2013-09-16 19:57:18 -0400

    First commit test

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-Welcome to p1!
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Tarek Aloch</title>
+	<meta charset="utf-8">
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+
+	<body>
+	<h1>Hello, world!</h1>
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	</body>
+
+</html>
