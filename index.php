5fd80d5bd761550a202cc71c50f8e66e7717a140
TarekAloch <taloch@fas.harvard.edu>
2013-09-16 23:20:06 -0400

    Test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	
 		<style>
 	
-			h1 {font-size: 50px; color:black; font-family: courier;}
+			h1 { font-size: 50px; color:black; font-family: courier; text-align: center}
 			body{background-color:#b0c4de; font-family: courier;}
 			b {font-size: 25px;}
 		</style>
@@ -14,13 +14,13 @@
 
 
 	<body>
-		<center>
+		
 		<h1>Welcome!</h1>
-		</center>
+		
 
-		<b><p>Bio</p></b>
-		<p align=left>My name is Tarek Aloch.<br><br> My CSS skills leave a lot to be desired but I am
-		quickly getting the hang of this! I have previously worked for Logistiq Corporation, a 
+		<p><b>Bio</b></p>
+		<p>My name is Tarek Aloch.<br><br> My CSS skills leave a lot to be desired but I am
+		quickly getting the hang of this! I have previously worked for <a href="http://www.logistiq.org">Logistiq Corporation</a>, a 
 		startup based out of central Massachusetts. My work and experience at Logistiq has compelled me
 		to build my web development skills. I was recently hired at Bose Corporation.  
 		<br><br>
@@ -30,10 +30,9 @@
 		<b>Code Editor</b>
 		<p>I'm a big fan of Sublime Text 3.</p>
 
-		<center>
-		<a href="http://www.logistiq.org"><img src="http://logistiq.org/images/logistiq.jpg"/> </a>
-		<p> Click the above image to visit Logistiq's home page.</p>
-		</center>
+		
+		
+		
 	</body>
 
 </html>
