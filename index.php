fab978cd20c6487bdac9088693a215e147033f16
TarekAloch <taloch@fas.harvard.edu>
2013-09-16 22:48:42 -0400

    Update to Project 1/index.php

--- index.php
+++ index.php
@@ -1,16 +1,39 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<title>Tarek Aloch</title>
-	<meta charset="utf-8">
-	<!-- Bootstrap -->
-	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<meta charset="utf-8">
+		<title>Tarek Aloch - Project 1</title>
+	
+		<style>
+	
+			h1 {font-size: 50px; color:black; font-family: courier;}
+			body{background-color:#b0c4de; font-family: courier;}
+			b {font-size: 25px;}
+		</style>
 	</head>
 
+
 	<body>
-	<h1>Hello, world!</h1>
-	<script src="http://code.jquery.com/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+		<center>
+		<h1>Welcome!</h1>
+		</center>
+
+		<b><p>Bio</p></b>
+		<p align=left>My name is Tarek Aloch.<br><br> My CSS skills leave a lot to be desired but I am
+		quickly getting the hang of this! I have previously worked for Logistiq Corporation, a 
+		startup based out of central Massachusetts. My work and experience at Logistiq has compelled me
+		to build my web development skills. I was recently hired at Bose Corporation.  
+		<br><br>
+		A major influence of mine is <a href="http://paulgraham.com/">Paul Graham</a>. He's the founder of Y-Combinator and 
+		<a href="https://news.ycombinator.com/news/">Hacker News</a>. </p> 
+		
+		<b>Code Editor</b>
+		<p>I'm a big fan of Sublime Text 3.</p>
+
+		<center>
+		<a href="http://www.logistiq.org"><img src="http://logistiq.org/images/logistiq.jpg"/> </a>
+		<p> Click the above image to visit Logistiq's home page.</p>
+		</center>
 	</body>
 
 </html>
